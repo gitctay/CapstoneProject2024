5c1eb282f0a23c246b5b3c989ddb43b258d236ab
ChristianT <73606323+GoodGuyDoc@users.noreply.github.com>
2024-09-19 21:05:10 -0400

    Added Project Structure

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
-
 if __name__ == '__main__':
     print("Hello World!")
     r = requests.get("https://www.charlotte.edu/")
     if r.status_code != 200:
         print("Something went wrong!")
+
