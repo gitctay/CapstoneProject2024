a7d80c79033d21324feedcde69fa56e9c919dcf5
gitctay <christiantay0113@gmail.com>
2024-09-27 10:49:27 -0400

    created basic selenium driver setup.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import requests
+from selenium import webdriver
+driver = webdriver.Chrome()
 if __name__ == '__main__':
     print("Hello World!")
-    r = requests.get("https://www.charlotte.edu/")
-    if r.status_code != 200:
-        print("Something went wrong!")
+    driver.get('https://www.charlotte.edu/')
+    print(driver.title)
+
+
+
 
