39375fd789f16742e5efd36bd8b544eb45ed01a4
gitctay <christiantay0113@gmail.com>
2024-09-30 17:39:26 -0400

    modified logging and started basic campusScraping

--- generalScrapingMod/Scripts/uncc-event-collection.py
+++ generalScrapingMod/Scripts/uncc-event-collection.py
@@ -1,14 +1,40 @@
 from selenium.webdriver import Chrome
-from selenium.common.exceptions import TimeoutException,NoSuchElementException,NoSuchAttributeException
-
-
+from selenium.common.exceptions import TimeoutException,NoSuchElementException,NoSuchAttributeException,ElementClickInterceptedException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 MAIN_SITE = "https://campusevents.charlotte.edu/"
-
+SUB_SITE = "https://campusevents.charlotte.edu/calendar"
 driver = Chrome()
+wait_for_element = WebDriverWait(driver, 10)
 
-def loadSite():
+
+def load_site():
     try:
-        driver.get(MAIN_SITE)
+        driver.get(SUB_SITE)
     except TimeoutException:
         print(f"Loading took too long for site {MAIN_SITE}")
+
+    # try:
+    #     upcoming_events = driver.find_element(By.XPATH,".//a[contains(text(),'Upcoming')]")
+    #     upcoming_events.click()
+    # except (NoSuchElementException,ElementClickInterceptedException) as ex:
+    #     #log a warning here
+    #     print(f"There was an exception {ex}")
+
+    events = driver.find_elements(By.XPATH, ".//div[contains(@class,'em-event-instance')]")
+    if len(events) == 0:
+        print(f"No events found on site {SUB_SITE}")
+    else:
+        for event in events:
+            #Will get the general text div that we can then parse
+            try:
+                event_text = event.find_element(By.XPATH,'.//div[@class="em-card_text"]')
+                event_title = event.find_element(By.XPATH,".//div[@class='em-card_text']//h3/a").text
+                print(event_title)
+            except Exception as ex:
+                print(ex)
+load_site()
+
+
