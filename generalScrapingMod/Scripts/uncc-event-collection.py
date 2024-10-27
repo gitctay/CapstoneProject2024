d61b8c9929d81566cd6ea5cbcb8be2fa4509305f
gitctay <christiantay0113@gmail.com>
2024-10-27 14:51:07 -0400

    added pagination and logger fixes.

--- generalScrapingMod/Scripts/uncc-event-collection.py
+++ generalScrapingMod/Scripts/uncc-event-collection.py
@@ -1,20 +1,16 @@
-from selenium.webdriver.remote.remote_connection import LOGGER
-
-from loggingTest import log_setup
-
-
+import time
 from selenium.common import ElementNotVisibleException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver import Chrome
-from selenium.common.exceptions import TimeoutException,NoSuchElementException,NoSuchAttributeException,ElementClickInterceptedException
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.wait import WebDriverWait
-
-from pymongo_insert import insert_event_data
+from generalScrapingMod.Scripts.logging_setup import log_setup
 
 MAIN_SITE = "https://campusevents.charlotte.edu/"
 SUB_SITE = "https://campusevents.charlotte.edu/calendar"
+
 
 def find_element_casual(driver: WebDriver | WebElement, locator, locator_string):
     try:
@@ -31,40 +27,64 @@ def load_site():
         print(f"Loading took too long for site {MAIN_SITE}")
 
 
-def run_event_collection(logger):
-    logger = log_setup('event_collection_log.txt')
+def run_event_collection():
     global driver
+    is_last_page = False
+    logger = log_setup('event_collection_log.txt')
     driver = Chrome()
+    load_site()
     wait_for_element = WebDriverWait(driver, 10)
-    events = driver.find_elements(By.XPATH, ".//div[contains(@class,'em-event-instance')]")
-    if len(events) == 0:
-        print(f"No events found on site {SUB_SITE}")
-    else:
+    max_page_elem = driver.find_element(By.XPATH,
+                                        '(//a[@class="em-pagination-item" and not(contains(@class, "em-pagination-item arrow"))])[last()]')
+    max_page_num = int(max_page_elem.get_attribute("innerText").strip())
+    current_page_num = 1
+
+    while current_page_num <= max_page_num:
+        events = driver.find_elements(By.XPATH, ".//div[contains(@class,'em-event-instance')]")
+        if len(events) == 0:
+            logger.warning(f"No events found on site {SUB_SITE}")
+            break
+        next_page_elem = find_element_casual(driver, By.XPATH, ".//a[@aria-label='Next page']")
         for event in events:
-            #Will get the general text div that we can then parse
+
+            # Will get the general text div that we can then parse
             try:
-                event_text = event.find_element(By.XPATH,'.//div[@class="em-card_text"]')
-                event_a_tag = event.find_element(By.XPATH,'.//div[@class="em-card_text"]//h3/a')
+
+                event_text = event.find_element(By.XPATH, './/div[@class="em-card_text"]')
+                event_a_tag = event.find_element(By.XPATH, './/div[@class="em-card_text"]//h3/a')
+                event_link = event_a_tag.get_attribute('href')
                 event_title = event_a_tag.text.strip()
-                event_date = event.find_element(By.XPATH,"(.//p[@class='em-card_event-text'])[1]").text.strip()
-                #all of the event-meeting data tags are located in the second index.
-                event_meeting = find_element_casual(event,By.XPATH,'.//p[@class="em-card_event-text"][2]')
+                event_date = event.find_element(By.XPATH, "(.//p[@class='em-card_event-text'])[1]").text.strip()
+                # all of the event-meeting data tags are located in the second index.
+                event_meeting = find_element_casual(event, By.XPATH, './/p[@class="em-card_event-text"][2]')
                 event_dict = {
                     "event_text": event_text,
                     "event_title": event_title,
                     "event_date": event_date,
                     "event_meeting": event_meeting,
-                    "event_a_tag": event_a_tag
+                    "event_link": event_link
                 }
                 if event_meeting is None:
-                    print("The event meeting location is not found Defaulting to None")
+                    logger.info("The event meeting location is not found Defaulting to None")
                 else:
-                    print(f'The event {event_title} will be held at {event_date} at {str(event_meeting.text)}')
+                    logger.info(f'The event {event_title} will be held at {event_date} at {str(event_meeting.text)}')
 
-                insert_event_data(event_dict)
+                # insert_event_data(event_dict)
 
-            except (ElementNotVisibleException,NoSuchElementException) as ex:
-                logger.error(f'There was an issue grabbing an element because the element is not visible or does not exist --> {ex}')
+            except (ElementNotVisibleException, NoSuchElementException) as ex:
+                logger.error(
+                    f'There was an issue grabbing an element because the element is not visible or does not exist --> {ex}')
+                continue
             except Exception as ex:
                 logger.error(f'There was an issue grabbing an element because of an unknown exception {ex}')
+                continue
+        current_page_num += 1
+        if next_page_elem is not None:
+            next_page_elem.click()
+            time.sleep(5)
+    return True
+
+
+def test_event_collect():
+    assert run_event_collection() == True
 
--- pymongo_insert.py
+++ pymongo_insert.py
@@ -6,12 +6,13 @@ def insert_event_data(event_dict):
     dbname = get_database()
     event_data_table = dbname["event_data"]
     item = {
-        "event_title": event_dict.event_title,
-        "event_date": event_dict.event_date,
-        "event_meeting": event_dict.event_meeting,
-        "event_text": event_dict.event_text,
-        "event_a_tag": event_dict.event_a_tag
+        "event_title": event_dict.get("event_title"),
+        "event_date": event_dict.get("event_date"),
+        "event_meeting": event_dict.get("event_meeting"),
+        "event_text": event_dict.get("event_text"),
+        "event_link": event_dict.get("event_link"),
     }
+
     event_data_table.insert_one(item)
 
 def add_dummy_data():
@@ -74,5 +75,5 @@ if __name__ == "__main__":
     update_dict = {"$set": {"event_title": "new title"}}
     add_dummy_data()
     query_event_data()
-    delete_event_data(event_dict)
+    # delete_event_data(event_dict)
     update_event_data()
