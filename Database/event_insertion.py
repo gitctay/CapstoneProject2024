f471ac285cd319f659ff6bd2e1b7efb4fa7b9ce5
Prasham <ppipaliy@uncc.edu>
2024-10-31 18:56:35 -0400

    added time stamp to store current time and delete data

--- Database/event_insertion.py
+++ Database/event_insertion.py
@@ -1,22 +1,31 @@
 # Get the database using the method we defined in pymongo_test_insert file
 from Database.pymongo_get_database import get_database
+from datetime import datetime, timedelta
+
+dbname = get_database()
+event_data_table = dbname["event_data"]
 
 
 def insert_event_data(event_dict):
-    dbname = get_database()
-    event_data_table = dbname["event_data"]
-    item = {
+    event = {
         "event_title": event_dict.get("event_title"),
         "event_date": event_dict.get("event_date"),
         "event_meeting": event_dict.get("event_meeting"),
         "event_link": event_dict.get("event_link")
     }
 
+    event_data_table.insert_one(event)
+
+def insert_date_time():
+    item = {
+        "lastAddedAt": datetime.now(),
+        "expireAt": datetime.now() + timedelta(days=7)
+    }
     event_data_table.insert_one(item)
 
+event_data_table.create_index("expireAt", expireAfterSeconds=0)
+
 def add_dummy_data():
-    dbname = get_database()
-    event_data_table = dbname["event_data"]
     item = {
         "event_title": "test",
         "event_date": "2021-07-13T00:00:00.000Z",
@@ -43,8 +52,6 @@ def add_dummy_data():
 
 # Event Pull Method - COMPLETE
 def query_event_data():
-    dbname = get_database()
-    event_data_table = dbname["event_data"]
     events = event_data_table.find().limit(5)  # Limit to the latest 5 events
 
     event_list = []
@@ -58,15 +65,10 @@ def query_event_data():
     return event_list
 
 def update_event_data():
-    dbname = get_database()
-    event_data_table = dbname["event_data"]
-
     result = event_data_table.update_one({"event_title": "test_update"}, {"$set": {"event_title": "title updated"}})
     print(f"Matched {result.matched_count} documents and modified {result.modified_count} documents")
 
 def delete_event_data(event_dict):
-    dbname = get_database()
-    event_data_table = dbname["event_data"]
     event_data_table.delete_one({"event_title": event_dict.event_title})
 
 if __name__ == "__main__":
--- Database/parking_insertion.py
+++ Database/parking_insertion.py
@@ -1,10 +1,10 @@
 # Get the database using the method we defined in pymongo_test_insert file
 from pymongo_get_database import get_database
 
+dbname = get_database()
+parking_data_table = dbname["parking_data"]
 
 def insert_parking_data(parking_dict):
-    dbname = get_database()
-    parking_data_table = dbname["parking_data"]
     item = {
         "parking_name": parking_dict.get("parking_name"),
         "availability": parking_dict.get("availability")
@@ -13,8 +13,6 @@ def insert_parking_data(parking_dict):
     parking_data_table.insert_one(item)
 
 def add_dummy_data():
-    dbname = get_database()
-    parking_data_table = dbname["parking_data"]
     item = {
         "parking_name": "East Deck",
         "availability": "50",
@@ -30,8 +28,6 @@ def add_dummy_data():
     parking_data_table.insert_many([item, item2, item3])
 
 def query_parking_data():
-    dbname = get_database()
-    parking_data_table = dbname["parking_data"]
     parking_data = parking_data_table.find().limit(5)
     parking_list = []
     for parking in parking_data:
@@ -55,15 +51,10 @@ def query_parking_data():
     #   print(event)
 
 def update_parking_data():
-    dbname = get_database()
-    parking_data_table = dbname["parking_data"]
-
     result = parking_data_table.update_one({"parking_name": "test_update"}, {"$set": {"parking_name": "title updated"}})
     print(f"Matched {result.matched_count} documents and modified {result.modified_count} documents")
 
 def delete_parking_data(parking_dict):
-    dbname = get_database()
-    parking_data_table = dbname["parking_data"]
     parking_data_table.delete_one({"parking_name": parking_dict.event_title})
 
 if __name__ == "__main__":
