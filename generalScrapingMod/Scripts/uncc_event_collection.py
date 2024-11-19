dad5f55eb5c9e25a3cfea411e45dd7a9aa02f0cd
gitctay <156626254+gitctay@users.noreply.github.com>
2024-11-19 16:08:26 -0500

    Update uncc_event_collection.py to be the most up to date version

--- generalScrapingMod/Scripts/uncc_event_collection.py
+++ generalScrapingMod/Scripts/uncc_event_collection.py
@@ -1,12 +1,10 @@
 import time
-
 from selenium.common import ElementNotVisibleException
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.wait import WebDriverWait
 from generalScrapingMod.Scripts.logging_setup import log_setup
 from database.event_insertion import insert_event_data
@@ -32,12 +30,12 @@ def load_site():
 
 def run_event_collection():
     global driver
+    is_last_page = False
     logger = log_setup('event_collection_log.txt')
     driver = Chrome()
+    print("Starting event collection...")
     load_site()
     wait_for_element = WebDriverWait(driver, 10)
-    
-    # Get maximum number of pages for events
     max_page_elem = driver.find_element(By.XPATH,
                                         '(//a[@class="em-pagination-item" and not(contains(@class, "em-pagination-item arrow"))])[last()]')
     max_page_num = int(max_page_elem.get_attribute("innerText").strip())
@@ -50,13 +48,23 @@ def run_event_collection():
             break
         next_page_elem = find_element_casual(driver, By.XPATH, ".//a[@aria-label='Next page']")
         for event in events:
+
+            # Will get the general text div that we can then parse
             try:
+
                 event_text = event.find_element(By.XPATH, './/div[@class="em-card_text"]')
                 event_a_tag = event.find_element(By.XPATH, './/div[@class="em-card_text"]//h3/a')
                 event_link = event_a_tag.get_attribute('href')
                 event_title = event_a_tag.text.strip()
                 event_date = event.find_element(By.XPATH, "(.//p[@class='em-card_event-text'])[1]").text.strip()
+                # all of the event-meeting data tags are located in the second index.
                 event_meeting = find_element_casual(event, By.XPATH, './/p[@class="em-card_event-text"][2]')
+                event_recurring = find_element_casual(event,By.XPATH,"//div[@class='recurring']")
+
+                if event_recurring is not None:
+                    event_recurring = True
+                else:
+                    event_recurring = False
 
                 if event_meeting is None:
                     logger.info("The event meeting location is not found Defaulting to None")
@@ -69,11 +77,13 @@ def run_event_collection():
                     "event_title": event_title,
                     "event_date": event_date,
                     "event_meeting": event_meeting,
-                    "event_link": event_link
+                    "event_link": event_link,
+                    "is_recurring": event_recurring
                 }
 
-                insert_event_data(event_dict)
 
+                insert_event_data(event_dict)
+                print('Added Event Data to DB')
             except (ElementNotVisibleException, NoSuchElementException) as ex:
                 logger.error(
                     f'There was an issue grabbing an element because the element is not visible or does not exist --> {ex}')
@@ -85,22 +95,15 @@ def run_event_collection():
         if next_page_elem is not None:
             next_page_elem.click()
             time.sleep(5)
-    
-    driver.quit()  # Close the browser after collection
+    print("Event collection completed.")
     return True
 
-# Function to run the event collection and log its success
-def job():
-    print("Starting event collection...")
-    run_event_collection()
-    print("Event collection completed.")
 
-# Schedule the job every 12 hours
-# schedule.every(12).hours.do(job)
 
-if __name__ == "__main__":
-    # Initial run
-    job()  # Run immediately on startup
-    while True:
-        # schedule.run_pending()  # Keep checking for scheduled jobs
-        time.sleep(1)  # Wait for a while before checking again
+
+
+def test_event_collect():
+    assert run_event_collection() == True
+
+
+
