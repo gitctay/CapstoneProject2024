ffd3ec5c3b189cc1b6080407109fccdd37e641e7
gitctay <156626254+gitctay@users.noreply.github.com>
2024-11-19 19:50:56 -0500

    Update uncc_event_collection.py
    Added headless

--- generalScrapingMod/Scripts/uncc_event_collection.py
+++ generalScrapingMod/Scripts/uncc_event_collection.py
@@ -8,6 +8,7 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.wait import WebDriverWait
 from generalScrapingMod.Scripts.logging_setup import log_setup
 from database.event_insertion import insert_event_data
+from selenium.webdriver.chrome.options import Options
 
 MAIN_SITE = "https://campusevents.charlotte.edu/"
 SUB_SITE = "https://campusevents.charlotte.edu/calendar/week?card_size=small&order=date&experience="
@@ -30,9 +31,11 @@ def load_site():
 
 def run_event_collection():
     global driver
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
     is_last_page = False
     logger = log_setup('event_collection_log.txt')
-    driver = Chrome()
+    driver = Chrome(options=chrome_options)
     print("Starting event collection...")
     load_site()
     wait_for_element = WebDriverWait(driver, 10)
@@ -40,7 +43,6 @@ def run_event_collection():
                                         '(//a[@class="em-pagination-item" and not(contains(@class, "em-pagination-item arrow"))])[last()]')
     max_page_num = int(max_page_elem.get_attribute("innerText").strip())
     current_page_num = 1
-
     while current_page_num <= max_page_num:
         events = driver.find_elements(By.XPATH, ".//div[contains(@class,'em-event-instance')]")
         if len(events) == 0:
@@ -48,10 +50,8 @@ def run_event_collection():
             break
         next_page_elem = find_element_casual(driver, By.XPATH, ".//a[@aria-label='Next page']")
         for event in events:
-
             # Will get the general text div that we can then parse
             try:
-
                 event_text = event.find_element(By.XPATH, './/div[@class="em-card_text"]')
                 event_a_tag = event.find_element(By.XPATH, './/div[@class="em-card_text"]//h3/a')
                 event_link = event_a_tag.get_attribute('href')
@@ -72,7 +72,6 @@ def run_event_collection():
                 else:
                     logger.info(f'The event {event_title} will be held at {event_date} at {(event_meeting)}')
                     event_meeting = event_meeting.get_attribute('innerText').strip()
-
                 event_dict = {
                     "event_title": event_title,
                     "event_date": event_date,
@@ -80,8 +79,6 @@ def run_event_collection():
                     "event_link": event_link,
                     "is_recurring": event_recurring
                 }
-
-
                 insert_event_data(event_dict)
                 print('Added Event Data to DB')
             except (ElementNotVisibleException, NoSuchElementException) as ex:
@@ -95,16 +92,12 @@ def run_event_collection():
         if next_page_elem is not None:
             next_page_elem.click()
             time.sleep(5)
+    driver.quit()
     print("Scraping completed.")
-    
     return True
-
-
-
-
 
 def test_event_collect():
     assert run_event_collection() == True
 
-
-
+if __name__ == '__main__':
+    run_event_collection()
